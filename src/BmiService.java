9a3f23cfa7fc210e965663232fb87a2f30938f72
Lera <lera1138@yandex.ru>
2023-09-17 00:22:55 +0300

    change BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,7 @@
 import java.lang.module.FindException;
 
 public class BmiService {
-    public double calculate(double heightMetr, int weightKg)
-    {
+    public double calculate(double heightMetr, int weightKg) {
         double bmi = weightKg / Math.pow(heightMetr, 2);
         return bmi;
 
